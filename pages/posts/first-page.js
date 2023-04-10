e0dff8df26a8a7a8c0e353349194c335c92f49d2
hajueun <dntru8520@naver.com>
2023-04-10 18:28:48 +0900

    First Commit

---
+++ pages/posts/first-page.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+export default function FirstPage() {
+    return (
+        <div>
+            <h2>Hello my name is hajueun</h2> <p>My hobby is watching baseball</p> <h1><p>And My favorite team is Lotte Giants</p></h1> <p>Thank you</p>
+            <Link href="/">Back to home</Link> 
+        </div>
+    )
+}
